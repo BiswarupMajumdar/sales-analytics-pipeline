ffb8bbe6cf31cd2945925dec5dd76a2ff61ec1ff
Biswarup Majumdar <biswarupmajumdar500@gmail.com>
2026-05-21 22:40:56 +0530

    done

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import sys
 from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+sys.path.append(str(ROOT_DIR))
+
 from main import main
 
 # -----------------------------
